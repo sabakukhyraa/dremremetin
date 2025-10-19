ee9ee5cc14b5b836a27ba9d8897ced0063d68b53
sabakukhyraa <alikerematas@gmail.com>
2025-10-19 16:14:47 +0300

    Rm node_modules and add gitignore

--- src/components/VideoLazyLoad.jsx
+++ src/components/VideoLazyLoad.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import LazyLoad from "react-lazyload";
+import { useInView } from "react-intersection-observer";
 
 const VideoLazyLoad = ({ src, type, className }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "100px",
+  });
+
   return (
-    <LazyLoad height={200} offset={100}>
-      <div className={`video ${className}`}>
-        <video className="rounded-md object-cover w-full h-52 xl:h-96" autoPlay loop muted>
+    <div ref={ref} className={`video ${className}`}>
+      {inView && (
+        <video
+          className="rounded-md object-cover w-full h-52 xl:h-96"
+          autoPlay
+          loop
+          muted
+        >
           <source src={src} type={type} />
           Your browser does not support the video tag.
         </video>
-      </div>
-    </LazyLoad>
+      )}
+    </div>
   );
 };
 
