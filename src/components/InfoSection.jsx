2da0b7f6223a4360c9c14f3136a14115c67da18c
sabakukhyraa <alikerematas@gmail.com>
2025-10-21 16:52:47 +0300

    Increase height of image container in InfoSection component for better layout

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -22,7 +22,7 @@ export default function InfoSection({
     >
       <div className="grid w-full xl:grid-cols-2 gap-0">
         {/* Image Container */}
-        <div className="relative h-64 xl:h-[530px] overflow-hidden">
+        <div className="relative h-64 xl:h-[550px] overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-t from-sky-900/40 via-transparent to-transparent"></div>
           <img
             loading="lazy"
