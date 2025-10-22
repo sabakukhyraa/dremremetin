79d48190293f87497a501180f1bbb9c81265194a
sabakukhyraa <alikerematas@gmail.com>
2025-10-22 10:24:24 +0300

    Refactor HomeSlider component layout and improve styling for slider text elements

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -6,17 +6,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { useContext } from "react";
 
-
 export default function HomeSlider() {
-
   const { language } = useContext(StateContext);
 
   const slideMapper = homeSliderData[language].map((slider, index) => (
     <SwiperSlide className="relative slider-image" key={index}>
-      <img loading="lazy"  className="" src={slider.image} alt={`slider${index}`} />
-      <div className="absolute text-start text-sky-50 flex flex-col justify-center items-start container w-full h-full z-[2] slider">
-        <h2 className="xl:large-title w-1/2 xl:w-full font-semibold text-2xl pb-6 xl:pb-12 mix-blend-difference">{slider.title}</h2>
-        <p className="font-light text-lg w-1/2 xl:w-full xl:text-2xl mix-blend-difference">
+      <img
+        loading="lazy"
+        className=""
+        src={slider.image}
+        alt={`slider${index}`}
+      />
+      <div className="absolute text-start text-sky-50 flex flex-col justify-center items-start container w-full h-full z-[2] slider px-8 md:px-12">
+        <h2 className="text-2xl lg:text-3xl 2xl:medium-title w-3/4 xl:w-3/4 font-semibold pb-6 2xl:pb-12 mix-blend-difference">
+          {slider.title}
+        </h2>
+        <p className="font-light text-md xl:text-xl w-3/4 xl:w-3/4 mix-blend-difference">
           {slider.article}
         </p>
       </div>
@@ -24,9 +29,7 @@ export default function HomeSlider() {
   ));
 
   return (
-    <div 
-    className="overflow-hidden !box-border relative h-[50vh] xl:h-[80vh] -mt-16 shadow-md xl:shadow-xl"
-    >
+    <div className="overflow-hidden !box-border relative h-[50vh] xl:h-[80vh] -mt-16 shadow-md xl:shadow-xl">
       <Swiper
         // className="!overflow-hidden h-full w-full text-xl xl:text-7xl"
         slidesPerView={1}
